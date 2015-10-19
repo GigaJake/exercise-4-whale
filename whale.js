84433e1f71bf3f66a994be68a127c09886d97f0c
GigaJake <GigaJake@gmail.com>
2015-10-19 14:45:13 -0500

    Eighth Commit
    I have a funny feeling about this one

--- whale.js
+++ whale.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($){
 	var num = parseInt(location.hash.substr(1));
 	console.log(num);
+
+	$.get('/whale/txt/' + num + '.txt', function(txt)) {
+		console.log(txt);
+	});
+
 	if(isNaN(num))
 	{
 		num = 0;
